c3b0595e8ab972c494f6788be7c4b62834674f91
Nicci Williams <nicci@nicciwill.com>
2017-12-21 15:18:21 -0500

    finished action tests

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,5 +1,26 @@
-describe('', () => {
-  xit('', () => {
-        
-  });
+import * as actions from './actions';
+
+describe('generateAuralUpdate', () => {
+    it('should return the action', () => {
+        const action = actions.generateAuralUpdate();
+        expect(action.type).toEqual(actions.GENERATE_AURAL_UPDATE);
+    });
 });
+
+describe('restartGame', () => {
+    it('should return the action with correctAnswer', () => {
+        const correctAnswer = 16;
+        const action = actions.restartGame(correctAnswer);
+        expect(action.type).toEqual(actions.RESTART_GAME);
+        expect(action.correctAnswer).toEqual(correctAnswer);
+    });
+});
+
+describe('makeGuess', () => {
+    it('should return the action with guess', () => {
+        const guess = 80;
+        const action = actions.makeGuess(guess);
+        expect(action.type).toEqual(actions.MAKE_GUESS);
+        expect(action.guess).toEqual(guess);
+    })
+})
